9f7e3bdf3c6732dc8429fb15c10cebf6976e2fa5
BestPig <bestpig82@gmail.com>
2015-02-01 03:04:41 +0100

    SystemUI: Add USB tethering QS tile
    Author: BestPig <bestpig82@gmail.com>
    Date:   Sun Feb 1 03:04:41 2015 +0100
    
        SystemUI: add USB Tether tile
    
         * Added vector drawables (blunden)
    
         * Long click action changed to open Tethering options, in the same way
           as Google does for Hotspot tile (bgcngm)
    
         * USB Tethering enablement check refactored (bgcngm and luca020400)
    
        Change-Id: Ic44f2b596fe32152f3f722347a855325c52113c6
    
    Author: Gabriele M <moto.falcon.git@gmail.com>
    Date:   Fri Apr 27 20:51:42 2018 +0200
    
        SystemUI: Toggle USB tethering only when USB is connected
    
        Pressing the tile when unplugged causes USB tethering to turn on
        as soon as the cable is connected. This can happen long after the
        tile has been pressed, so don't do anything if unplugged.
    
        Change-Id: Ib8bbdc758097ca7af5d4e7c3483c28ba42fb9ce1
    
    [ghostrider-reborn] Updated for android 12
    
    Change-Id: Ibbc0ba9de2cc3eee2a7f6a9c9f005730244f0b3a
    Co-authored-by: Adithya <gh0strider.2k18.reborn@gmail.com>

--- packages/SystemUI/src/com/android/systemui/qs/tileimpl/QSFactoryImpl.java
+++ packages/SystemUI/src/com/android/systemui/qs/tileimpl/QSFactoryImpl.java
@@ -50,6 +50,7 @@ import com.android.systemui.qs.tiles.ReduceBrightColorsTile;
 import com.android.systemui.qs.tiles.RotationLockTile;
 import com.android.systemui.qs.tiles.ScreenRecordTile;
 import com.android.systemui.qs.tiles.UiModeNightTile;
+import com.android.systemui.qs.tiles.UsbTetherTile;
 import com.android.systemui.qs.tiles.UserTile;
 import com.android.systemui.qs.tiles.WifiTile;
 import com.android.systemui.qs.tiles.WorkModeTile;
@@ -95,6 +96,7 @@ public class QSFactoryImpl implements QSFactory {
     private final Provider<QuickAccessWalletTile> mQuickAccessWalletTileProvider;
     private final Provider<AODTile> mAODTileProvider;
     private final Provider<CaffeineTile> mCaffeineTileProvider;
+    private final Provider<UsbTetherTile> mUsbTetherTileProvider;
     private final Provider<DataSwitchTile> mDataSwitchTileProvider;
 
     private final Lazy<QSHost> mQsHostLazy;
@@ -133,6 +135,7 @@ public class QSFactoryImpl implements QSFactory {
             Provider<QuickAccessWalletTile> quickAccessWalletTileProvider,
             Provider<AODTile> aodTileProvider,
             Provider<CaffeineTile> caffeineTileProvider,
+            Provider<UsbTetherTile> usbTetherTileProvider,
             Provider<DataSwitchTile> dataSwitchTileProvider) {
         mQsHostLazy = qsHostLazy;
         mCustomTileBuilderProvider = customTileBuilderProvider;
@@ -166,6 +169,7 @@ public class QSFactoryImpl implements QSFactory {
         mQuickAccessWalletTileProvider = quickAccessWalletTileProvider;
         mAODTileProvider = aodTileProvider;
         mCaffeineTileProvider = caffeineTileProvider;
+        mUsbTetherTileProvider = usbTetherTileProvider;
         mDataSwitchTileProvider = dataSwitchTileProvider;
     }
 
@@ -237,6 +241,8 @@ public class QSFactoryImpl implements QSFactory {
                 return mAODTileProvider.get();
             case "caffeine":
                 return mCaffeineTileProvider.get();
+            case "usb_tether":
+                return mUsbTetherTileProvider.get();
             case "dataswitch":
                 return mDataSwitchTileProvider.get();
         }
