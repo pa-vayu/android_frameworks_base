c5609667569f18e2428c746d5d4ed79effbaea98
Tiger Huang <tigerhuang@google.com>
2022-04-10 17:24:58 +0800

    Send the attached frame to the client
    The attached frame is the frame of the parent window that its child
    attaches to, as long as the child is not
    TYPE_APPLICATION_ATTACHED_DIALOG. It is a factor while computing the
    window frame.
    
    Previously, we let the child obtain the attached frame from its parent
    view root. However, if the parent and the child are not in the same
    process, it can be tricky.
    
    This CL sends the attached frame from the server to the client via
    ClientWindowFrames. In this way, the child can compute its window frame
    without having to accessing the parent view root.
    
    Bug: 161810301
    Bug: 175861127
    Test: atest ActivityRecordTests WindowLayoutTests
    Change-Id: Ia844a4c927027e305a56114216eaee2bf7933b1f

--- services/core/java/com/android/server/wm/WindowManagerService.java
+++ services/core/java/com/android/server/wm/WindowManagerService.java
@@ -1449,7 +1449,7 @@ public class WindowManagerService extends IWindowManager.Stub
     public int addWindow(Session session, IWindow client, LayoutParams attrs, int viewVisibility,
             int displayId, int requestUserId, InsetsVisibilities requestedVisibilities,
             InputChannel outInputChannel, InsetsState outInsetsState,
-            InsetsSourceControl[] outActiveControls) {
+            InsetsSourceControl[] outActiveControls, Rect outAttachedFrame) {
         Arrays.fill(outActiveControls, null);
         int[] appOp = new int[1];
         final boolean isRoundedCornerOverlay = (attrs.privateFlags
@@ -1864,6 +1864,13 @@ public class WindowManagerService extends IWindowManager.Stub
 
             outInsetsState.set(win.getCompatInsetsState(), win.isClientLocal());
             getInsetsSourceControls(win, outActiveControls);
+
+            if (win.mLayoutAttached) {
+                outAttachedFrame.set(win.getParentWindow().getCompatFrame());
+            } else {
+                // Make this invalid which indicates a null attached frame.
+                outAttachedFrame.set(0, 0, -1, -1);
+            }
         }
 
         Binder.restoreCallingIdentity(origId);
